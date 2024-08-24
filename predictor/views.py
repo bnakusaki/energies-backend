cc899364b61ca5c5411d99868820ec30670ad13e
bnakusaki <akusakibenjaminnartey@gmail.com>
2024-08-24 15:18:39 +0000

    link model

--- predictor/views.py
+++ predictor/views.py
@@ -1,6 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from . import model
 
 # Create your views here.
 def index(request):
-    return HttpResponse("Hello, world. You're at the predictor index.")
+    # Extract the 'value' parameter from the GET request
+    value = request.GET.get('value', None)
+    
+    if value is not None:
+        prediction = model.predict(value)  # Assuming model has a predict function
+        return HttpResponse(prediction)
+    else:
+        return HttpResponse("No value provided.")
